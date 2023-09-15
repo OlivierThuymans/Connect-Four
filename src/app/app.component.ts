c364cdb4cf2f7b42027553f6a5325483533bbb08
Olivier Thuymans <olivier.thuymans@hotmail.com>
2023-09-15 20:23:46 +0200

    pilling tokens properly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,25 +33,46 @@ export class AppComponent {
   circleColor(y: number, x: number) {
     //console.log(y + 1, x + 1)
     let color: string;
-    let currentCircle = this.addedTokens.find(item => item.y === y && item.x === x)
+    let currentCircle = this.addedTokens.find(item => item.y === y + 1 && item.x === x + 1)
     !currentCircle ? color = 'e' : currentCircle.color === 'red' ? color = 'red' : currentCircle.color === 'yellow' ? color = 'yellow' : null;
     return color;
   }
 
+
+  findX(col: number) {
+    let number: number = 6;
+    
+    for(let i = 1; i <= 6; i++) {
+      let targetX = i;
+      let targetY = col;
+      let found = this.addedTokens.find(item => item.y === targetY && item.x === targetX)
+    
+      if (found) {
+        number = found.x - 1;
+        break;
+      }
+    }
+    return number;
+  }
+
   circleData(color: string, y: number, x: number) {
 
-    this.selectedToken !== '' ? console.log(color, y, x) : null;
-
     //const fieldName = `added${color.charAt(0).toUpperCase() + color.slice(1)}Tokens`;
+
+    let col = y + 1;
+    let lign = x + 1;
+
+    console.log(col, lign)
+    
+    this.findX(col);
 
     if(this.selectedToken !== '') {
       this.addedTokens.push({
         color: color,
-        y: y,
-        x: x
+        y: col,
+        x: this.findX(col)
       })
     }
-    
   }
 
   
